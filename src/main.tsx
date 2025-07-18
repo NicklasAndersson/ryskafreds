92699ebed1fe1314d9ac51b707dae27457bef620
Nicklas Andersson <nicklas@wwn.se>
2025-07-18 18:11:00 +0200

    struktur

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import HomePage from './HomePage';
+import ArticlePage from './ArticlePage';
+
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'nyheter/:slug',
+        element: <ArticlePage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
